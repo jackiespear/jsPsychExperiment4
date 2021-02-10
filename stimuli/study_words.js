b98615040455c899055494ee3a8d92b0ebb24cec
Jackie Spear <jackiespear44@gmail.com>
2021-02-10 13:22:20 -0600

    Adding Files

---
+++ stimuli/study_words.js
@@ -0,0 +1,26 @@
+studywords = [
+{"stimulus1":"<p style='font-size: 48px;'>EAGLE</p>","data":{"type":"old","category":"birds","word":"EAGLE"},
+"stimulus2":"<p style='font-size: 48px;'>ROBIN</p>","data":{"type":"old","category":"birds","word":"ROBIN"},
+"stimulus3":"<p style='font-size: 48px;'>BLUEJAY</p>","data":{"type":"old","category":"birds","word":"BLUEJAY"},
+"stimulus4":"<p style='font-size: 48px;'>CONCEPT</p>","data":{"type":"distinct","category":"birds","word":"CONCEPT"},
+"test":"<p style='font-size: 48px;'>EAGLE</p>","data":{"type":"old","category":"birds","test_type":"cat_test","word":"EAGLE"}
+},
+{"stimulus1":"<p style='font-size: 48px;'>BLUE</p>","data":{"type":"old","category":"colors","word":"BLUE"},
+"stimulus2":"<p style='font-size: 48px;'>RED</p>","data":{"type":"old","category":"colors","word":"RED"},
+"stimulus3":"<p style='font-size: 48px;'>GREEN</p>","data":{"type":"old","category":"colors","word":"GREEN"},
+"stimulus4":"<p style='font-size: 48px;'>TERRORISM</p>","data":{"type":"distinct","category":"colors","word":"TERRORISM"},
+"test":"<p style='font-size: 48px;'>GREEN</p>","data":{"type":"old","category":"colors","test_type":"cat_test","word":"GREEN"}
+},
+{"stimulus1":"<p style='font-size: 48px;'>GASOLINE</p>","data":{"type":"old","category":"fuel","word":"GASOLINE"},
+"stimulus2":"<p style='font-size: 48px;'>DIESEL</p>","data":{"type":"old","category":"fuel","word":"DIESEL"},
+"stimulus3":"<p style='font-size: 48px;'>OIL</p>","data":{"type":"old","category":"fuel","word":"OIL"},
+"stimulus4":"<p style='font-size: 48px;'>GATHER</p>","data":{"type":"distinct","category":"fuel","word":"GATHER"},
+"test":"<p style='font-size: 48px;'>DIESEL</p>","data":{"type":"old","category":"fuel","test_type":"cat_test","word":"DIESEL"}
+},
+{"stimulus1":"<p style='font-size: 48px;'>HOUSE</p>","data":{"type":"old","category":"dwelling","word":"HOUSE"},
+"stimulus2":"<p style='font-size: 48px;'>APARTMENT</p>","data":{"type":"old","category":"dwelling","word":"APARTMENT"},
+"stimulus3":"<p style='font-size: 48px;'>MANSION</p>","data":{"type":"old","category":"dwelling","word":"MANSION"},
+"stimulus4":"<p style='font-size: 48px;'>HIRE</p>","data":{"type":"distinct","category":"dwelling","word":"HIRE"},
+"test":"<p style='font-size: 48px;'>MYTH</p>","data":{"type":"new","category":"dwelling","test_type":"unrelated_test","word":"MYTH"}
+},
+]
